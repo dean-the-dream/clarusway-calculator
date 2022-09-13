ee60afde59fc5f90c5abca9d4b1eea44172b9746
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-13 11:38:45 -0400

    fix: update operation button functionality

--- calc.js
+++ calc.js
@@ -2,6 +2,7 @@ const screen = document.querySelector(".screen p"); //the section of the calcula
 let equation = [];  // to hold the entire quation until a calculation is performed
 const numberpad = document.querySelector(".numpad"); // the section that holds all of the keys
 const clearButton = document.querySelector(".clear");
+const equalButton = document.querySelector (".equal")
 let formattedNumber = ["0"]; //to hold the number the user entered until an operation is entered
 let operationIsPressed = false; // to track when an operation is entered
 function enterNumber (e) { //tells js what to do when numbers are entered, based on the state of the calculator
@@ -10,13 +11,12 @@ function enterNumber (e) { //tells js what to do when numbers are entered, based
         const digit = e.target.innerText; // digit represents the number the user entered
         if (operationIsPressed) {
             screen.innerText = "";
-            // operationIsPressed = false;
             resetOperation();
         }
-        if(formattedNumber.length < 10){
-            if(screen.innerText === "0"){
+        if(formattedNumber.length < 10){ //if the formatted number has less than 10 digits
+            if(screen.innerText === "0"){ // if is is equal to 0
                 if (digit === "0"){
-
+                    formattedNumber =[];
                 } else if (digit === ".") {
                     formattedNumber.push(digit)
                     screen.innerText += digit;
@@ -43,7 +43,7 @@ function enterNumber (e) { //tells js what to do when numbers are entered, based
 
             // equation.push(formattedNumber.join(""));
 
-
+            console.log(formattedNumber)
             clearButton.innerText = "C";
         }
 
@@ -61,9 +61,8 @@ function engageOperation(x) { //change the state of the calculator when the user
     resetOperation();
     if(!(x.className.includes("equal"))){
         x.style = `background-color: white; color: #FE9F06`
-        operationIsPressed = true; 
     }
-    
+    operationIsPressed = true; 
 }
 
 function pressClear (x) {
@@ -83,44 +82,54 @@ function pressClear (x) {
 
 function enterOperation (e) { //adds operation to equation. determines if quation is ready to be calculated
     if(e.target.className.includes("operation")){ 
-        engageOperation(e.target)
-        if(formattedNumber.length > 1)
+        engageOperation(e.target);
+        (formattedNumber[0] === "0") ? formattedNumber.shift() : null;
+        if(formattedNumber.length > 0){
+            equation.push(formattedNumber.join(""));
+            formattedNumber = [];
+        }
+        equation.push(e.target.innerText);
 
-        if(isNaN((equation[0]))){ // if the first array item is not a number
+        if(isNaN((parseFloat(equation[0])))){ // if the first array item is not a number
             equation.shift(); // remove the first item 
         } 
-        
-        equation.push(formattedNumber);
-        equation.push(e.target.innnerText);
-        // else if((e.target.innerText === "=") && equation.length > 1){ //if user pressed equal and there is more than one character
-        //     equation.push(e.target.innerText) // add equal to the equation array
-        //     calculate(equation); 
-        // } else if((equation.length > 2)){ // if if the equation has more than one item
-        //     if ((isNaN(equation[equation.length - 1]) && isNaN(equation[equation.length - 2]))) { //if the last two items are symbols
-        //         equation.splice(equation.length - 2,1) // remove the second to last item
-        //     } else if((equation[1] === "X") ||( equation[1] === "/")){ // if the second item is multiply or divide
-        //         calculate(equation);
-        //     } 
-        // } else if ( equation.lenghth > 3){
-        //     if((equation[equation.length - 1] === "+") || (equation[equation.length - 1] === "-")){ //if the last operation is a plus or a minus
-        //         calculate(equation);
-        //     } else if (equation.length > 4) {
-        //     calculate(equation)
-        //     }
-        // } 
-        
+        else if((e.target.innerText === "=") && equation.length > 1){ //if user pressed equal and there is more than one character
+            equation.push(e.target.innerText) // add equal to the equation array
+            calculate(equation); 
+        } else if((equation.length > 2)){ // if if the equation has more than one item
+            if ((isNaN(equation[equation.length - 1]) && isNaN(equation[equation.length - 2]))) { //if the last two items are symbols
+                equation.splice(equation.length - 2,1) // remove the second to last item
+            } else if((equation[1] === "X") ||( equation[1] === "/")){ // if the second item is multiply or divide
+                calculate(equation);
+            } 
+        } else if ( equation.lenghth > 3){
+            if((equation[equation.length - 1] === "+") || (equation[equation.length - 1] === "-")){ //if the last operation is a plus or a minus
+                calculate(equation);
+            } else if (equation.length > 4) {
+            calculate(equation)
+            }
+        } 
+        console.log(formattedNumber)
         console.log(equation)
     }
 }
 
 
 function calculate (x){
-    if(x.length > 2){
-        const result = twoNum(x)
-        screen.innerText = result;
+    if(x.length = 2){
+        result = equation[0];
         equation = [result];
-    }
+        formattedNumber = [];
+    } else {
+        result = "It Works";
+        equation = [result];
+        formattedNumber = []
 
+        // const result = twoNum(x)
+        // screen.innerText = result;
+        // equation = [result];
+    }
+    
 }
 
 function twoNum (x) {
