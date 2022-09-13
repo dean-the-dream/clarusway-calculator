5fb413ce0f04e7d608ba7c51a68eaa96ffa8be99
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-13 01:45:19 -0400

    fix: add animations to the operations

--- calc.js
+++ calc.js
@@ -1,4 +1,4 @@
-const screen = document.querySelector(".screen p");
+const screen = document.querySelector(".screen p"); //
 let display = "0";
 let equation = [];
 const numberpad = document.querySelector(".numpad");
@@ -6,12 +6,13 @@ const clearButton = document.querySelector(".clear");
 let formattedNumber = ["0"];
 let operationIsPressed = false;
 function enterNumber (e) {
-     
+
     if(e.target.className.includes("number")){
         const digit = e.target.innerText;
         if (operationIsPressed) {
             screen.innerText = "";
-            operationIsPressed = false;
+            // operationIsPressed = false;
+            resetOperation();
         }
         if(formattedNumber.length < 10){
             if(screen.innerText === "0"){
@@ -27,22 +28,22 @@ function enterNumber (e) {
             } else if(screen.innerText.includes(".")){
                 if(digit !== ".") {
                     formattedNumber.push(digit);
-                    screen.innerText += digit;  
+                    screen.innerText += digit;
                 }
             } else if((formattedNumber.length >= 3)){
                 formattedNumber.push(digit);
                 let displayNum = formattedNumber.join("");
                 let displayNum1 = Number(displayNum);
                 screen.innerText = displayNum1.toLocaleString("en-US");
-                
+
             } else {
                 screen.innerText += digit;
                 formattedNumber.push(digit)
             }
-            
-            
+
+
             // equation.push(formattedNumber.join(""));
-            
+
 
             clearButton.innerText = "C";
         }
@@ -50,17 +51,24 @@ function enterNumber (e) {
     }
 }
 
-// function resetOperation() {
-//     e.target.style = `background-color: #FE9F06;
-//                       color: white;`
-// }
+function resetOperation() {
+    let bttns = document.querySelectorAll(".orange");
+    for(i of bttns)
+    i.style = `background-color: #FE9F06; color: white;`
+    operationIsPressed = false;
+}
 
-// function engageOperation() {
-//     e.target.style = `background-color: white;`
-// }
+function engageOperation(x) {
+    resetOperation();
+    if(!(x.className.includes("equal"))){
+        x.style = `background-color: white; color: #FE9F06`
+        operationIsPressed = true; 
+    }
+    
+}
 
 function pressClear (x) {
-    if(x.target.className.includes("clear")){  
+    if(x.target.className.includes("clear")){
         if(clearButton.innerText == "C") {
             screen.innerText = "0"
             formattedNumber = [];
@@ -74,9 +82,10 @@ function pressClear (x) {
 }
 
 function enterOperation (e) {
-    if(e.target.className.includes("operation")){ 
-        operationIsPressed = true;
-        e.target.style = `background-color: white; color: #FE9F06 ` 
+    if(e.target.className.includes("operation")){
+        engageOperation(e.target)
+        // operationIsPressed = true;
+        // e.target.style = `background-color: white; color: #FE9F06 `
         isNaN(parseFloat(formattedNumber.join(""))) ?  null : equation.push(parseFloat(formattedNumber.join("")));
         equation.push(e.target.innerText)
         formattedNumber = [];
@@ -110,7 +119,7 @@ function twoNum (x) {
         case "/":
            return x[0] / x[2]
     }
-        
+
 }
 
 numberpad.addEventListener("click", enterNumber)
