7ccd4e42110494655d242b5ffea7e7779486e131
Dean-The-Dream <lawrence_dean@icloud.com>
2022-09-12 20:42:12 -0400

    fix: create clear screen function

--- calc.js
+++ calc.js
@@ -1,13 +1,30 @@
 const screen = document.querySelector(".screen p");
 const equation = []
 const numberpad = document.querySelector(".numpad")
+const clearButton = document.querySelector(".clear")
 function enterNumber (e) {
     console.log(e);
     if(e.target.className.includes("number")){
-        const digit = e.target.innerText
+        screen.innerText == "0" ? screen.innerText = "" : screen.innerText == "0";
+        const digit = e.target.innerText;
         equation.push(digit);
         screen.innerText += digit;
+
+        clearButton.innerText = "C";
+    }
+}
+
+function clearScreen (e) {
+    if(e.target.className.includes("clear")){   
+        screen.innerText = "0"
+        clearButton.innerText = "AC"
+    }
+}
+function operator () {
+    if(e.target.className.includes("number")){
+
     }
 }
 
 numberpad.addEventListener("click", enterNumber)
+numberpad.addEventListener("click", clearScreen)
